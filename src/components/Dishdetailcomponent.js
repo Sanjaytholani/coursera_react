ae6717ed7832b10c537835e85a08b0400b48c18b
Sanjaytholani <sanjaytholani.m@gmail.com>
2020-06-20 22:43:14 +0530

    Assignment-3

--- src/components/Dishdetailcomponent.js
+++ src/components/Dishdetailcomponent.js
@@ -1,8 +1,12 @@
 import React,{Component} from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-  CardTitle,Breadcrumb, BreadcrumbItem  } from 'reactstrap';
+  CardTitle,Breadcrumb, BreadcrumbItem,Button,Modal,ModalHeader,ModalBody, FormGroup,Form,Label  } from 'reactstrap';
 import {Link} from 'react-router-dom';
+import {Control,LocalForm,Errors} from 'react-redux-form';
 
+
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
 
     function RenderDish({dish}) {
         return(
@@ -22,7 +26,7 @@ import {Link} from 'react-router-dom';
                       {comment.comment}
                       <br></br><br></br>
                       --{comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                      <br></br><br></br>
+                      <br></br>
                   </li>
               );
           })
@@ -32,6 +36,9 @@ import {Link} from 'react-router-dom';
                   <ul className="list-unstyled">
                       {commentlist}
                   </ul>
+                  <CommentForm />
+                  <br/>
+                  <br/>
               </div>
           );
     }
@@ -63,5 +70,68 @@ import {Link} from 'react-router-dom';
       }
       else
       return(<div></div>);
+  }
+  class CommentForm extends Component{
+      constructor(props){
+          super(props);
+          this.toggleModal = this.toggleModal.bind(this);
+          this.state={
+              isModalOpen: false
+          };
+      }
+      toggleModal() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
+        });
+      }
+      handleSubmit(values) {
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+    }
+      render(){
+          return (
+              <React.Fragment>
+                <Button outline onClick={this.toggleModal}><span className="fa fa-pencil"></span> Submit Comment</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                  <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                  <ModalBody>
+                  <LocalForm onSubmit={(values)=>this.handleSubmit(values)}>
+                  <FormGroup>
+                      <Label htmlFor="rating">Rating</Label>
+                      <Control.select model=".rating" name="rating" id="rating" className="form-control">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                      </Control.select>
+                  </FormGroup>
+                  <FormGroup>
+                      <Label htmlFor="name">Your Name</Label>
+                      <Control.text model=".name" name="name" id="name" className="form-control"
+                      placeholder="Your Name" validators={{
+                        minLength: minLength(3),maxLength: maxLength(15)
+                    }}/>
+                    <Errors 
+                      className="text-danger"
+                      model=".name"
+                      show="touched"
+                      messages={{
+                          minLength: "Must be greater than 2 characters",
+                          maxLength: "Must be less than 15 characters"
+                      }}
+                      />
+                  </FormGroup>
+                  <FormGroup>
+                      <Label htmlFor="comment">Comment</Label>
+                      <Control.textarea model=".comment" name="comment" id="comment" rows="6" className="form-control"/>
+                  </FormGroup>
+                  <Button type="submit" className="bg-primary">Submit</Button>
+              </LocalForm>
+                  </ModalBody>
+              </Modal>
+              </React.Fragment>
+          );
+      }
   }
 export default DishDetail
